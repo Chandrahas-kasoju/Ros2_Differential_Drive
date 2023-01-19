0854244fdc532e93e6acafb82417c67106768548
Chandrahas-kasoju <114418431+Chandrahas-kasoju@users.noreply.github.com>
2023-01-20 00:41:45 +0100

    updated launch file

--- Diff_robot/robot_bringup/launch/test_parameters.launch.py
+++ Diff_robot/robot_bringup/launch/test_parameters.launch.py
@@ -1,17 +1,22 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    
+    config = os.path.join(
+        get_package_share_directory('robot_bringup'),
+        'config',
+        'param_vel_node.yaml'
+        )
     return LaunchDescription([
         Node(
             package='ros2_control_tests_nodes',
             executable='vel_pub',
-            name='vel_pub',
+            name='param_vel_node',
             output='screen',
             emulate_tty=True,
-            parameters=[
-                {'velocity_x': 0.2}
-            ]
+            parameters=[config]
         )
     ])
